2763140a51e081488f7b0bd0e97259e034a32162
Idalis Paz <idalispaz@gmail.com>
2020-06-17 02:56:51 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,13 +4,16 @@ def say_hello(name)
   puts "Hello #{name}!"
 end
 
+def say_hello("Kent Beck")
+  puts "Hello Kent Beck!"
+end
+
 say_hello("Kent Beck")
 
 
 def say_hello(name = "Ruby Programmer")
   puts "Hello Ruby Programmer!"
 end
-
 
 say_hello
 
